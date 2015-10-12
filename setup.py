53091ed161afee5b9f60764ea0ad8eb6384c497f
Sami <s.elahmadie@gmail.com>
2015-10-12 14:08:20 -0700

    Moving requirements to setup file

---
+++ setup.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+
+from distutils.core import setup
+
+setup(
+    name='LoadStone',
+    version='0.1',
+    description='Interface for FFXIV Lodestone',
+    author='Sami Elahmadie',
+    author_email='s.elahmadie@gmail.com',
+    url='https://github.com/Demotivated/loadstone/',
+    packages=['api'],
+    install_requires=[
+        'flask==0.10.1',
+        'flask_sqlalchemy==2.0,'
+        'lxml==3.4.4,'
+        'psycopg2==2.6.1,'
+        'requests==2.7.0',
+    ]
+)

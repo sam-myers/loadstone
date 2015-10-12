7792f2441bb8435ebc21a2a9e86236accc57d41a
Sami <s.elahmadie@gmail.com>
2015-10-12 14:23:12 -0700

    Fixing requirements formatting

--- setup.py
+++ setup.py
@@ -12,9 +12,9 @@ setup(
     packages=['api'],
     install_requires=[
         'flask==0.10.1',
-        'flask_sqlalchemy==2.0,'
-        'lxml==3.4.4,'
-        'psycopg2==2.6.1,'
+        'flask_sqlalchemy==2.0',
+        'lxml==3.4.4',
+        'psycopg2==2.6.1',
         'requests==2.7.0',
     ]
 )

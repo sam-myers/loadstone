d90d35063f1a79916c20d32d3634842dd59798f1
Sami <s.elahmadie@gmail.com>
2015-10-12 23:44:28 -0700

    Fix default fixture to initialize database

--- api/tests/conftest.py
+++ api/tests/conftest.py
@@ -3,5 +3,9 @@ import pytest
 
 @pytest.fixture(scope='module')
 def app():
-    from api import app
+    from api import app, db
+
+    app.config['TESTING'] = True
+    db.create_all()
+
     return app

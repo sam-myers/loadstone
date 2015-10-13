0b6e9d6e329b8e88fca9635640ef0842f3cb82c2
Sami <s.elahmadie@gmail.com>
2015-10-12 23:41:30 -0700

    Add scrape character unit test

--- api/tests/test_scrapers.py
+++ api/tests/test_scrapers.py
@@ -7,3 +7,14 @@ def test_scrape_item_by_id():
     assert item.name == 'Thyrus Zenith'
     assert item.type == 'Two-handed Conjurer\'s Arm'
     assert item.ilvl == 90
+
+
+def test_scrape_character_by_id():
+    from api.scrapers.character import scrape_character_by_id
+
+    name = scrape_character_by_id('8774791')
+
+    assert name.id == '8774791'
+    assert name.name == 'Mina Loriel'
+    assert name.species == 'Miqo\'te'
+    assert name.gender == 'Female'

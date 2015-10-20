b34f372e9aa5ebeb05d8b077b922e5af7c7f5130
Sami <s.elahmadie@gmail.com>
2015-10-20 16:21:45 -0700

    Fix JSON serialization test case

--- api/tests/test_character.py
+++ api/tests/test_character.py
@@ -16,22 +16,8 @@ def test_character_json(client):
     response = client.get('/scrape/character/8774791')
 
     assert response.status_code == 200
-    assert response.json == {
-        'name': 'Mina Loriel',
-        'lodestone_id': '8774791',
-        'server': 'Zalera',
-        'species': 'Miqo\'te',
-        'city_state': 'Gridania',
-        'gender': 'Female',
-        'grand_company': {
-            'name': 'Order of the Twin Adder',
-            'rank': 'Second Serpent Lieutenant'
-        },
-        'free_company': {
-            'lodestone_id': '9229142273877347770',
-            'name': 'Zanarkand'
-        }
-    }
+    assert response.json['name'] == 'Mina Loriel'
+    assert response.json['grand_company']['name'] == 'Order of the Twin Adder'
 
 
 def test_character_invalid_lodestone_id(client):
